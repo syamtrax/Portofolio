3e8d7c9d59bead9283d737cd92223ff937e8fa75
Satria Syammahestatma <satriasyamm@gmail.com>
2022-12-08 12:51:46 +0700

    init

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+
+function switchTheme(e) {
+  if (e.target.checked) {
+    document.documentElement.setAttribute("theme", "light");
+  } else {
+    document.documentElement.setAttribute("theme", "dark");
+  }
+}
+toggleSwitch.addEventListener("change", switchTheme, false);
